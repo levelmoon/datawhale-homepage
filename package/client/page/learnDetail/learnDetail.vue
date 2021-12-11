87268a970c812a0c538d35322f55ced0ccd724f6
shadowings-zy <shadowingszy@outlook.com>
2021-12-11 18:26:46 +0800

    feat: 相关视频页面完成

--- package/client/page/learnDetail/learnDetail.vue
+++ package/client/page/learnDetail/learnDetail.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="learn-detail">learnDetail</div>
+  <div class="learn-detail flex-row-left">
+    <el-menu :default-active="currentMenuIndex" class="learn-detail-menu">
+      <el-menu-item
+        v-for="(item, index) in chapterList"
+        :index="`chapter-${index}`"
+        :key="`learn-menu-${index}`"
+        @click="handleMenuItemClick(item.chapterId, index)"
+      >
+        <div>{{ item.title }}</div>
+      </el-menu-item>
+      <el-menu-item
+        v-if="videoList !== undefined && videoList.length > 0"
+        key="learn-menu-video"
+        index="chapter-video"
+        @click="handleVideoItemClick"
+      >
+        <div>相关视频</div>
+      </el-menu-item>
+    </el-menu>
+    <div v-if="!showVideo" class="learn-content">{{ content }}</div>
+    <div v-if="showVideo" class="learn-video flex-row-left">
+      <el-card
+        class="learn-video-card"
+        v-for="(item, index) in videoList"
+        :key="`learn-video-item-${index}`"
+        :body-style="{ padding: '0px', width: '300px', height: '260px' }"
+        @click="jumpToUrl(item.videoUrl)"
+      >
+        <img
+          class="learn-video-card-image"
+          :src="item.imageUrl"
+        />
+        <div class="learn-video-card-text">{{ item.title }}</div>
+      </el-card>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -15,10 +50,31 @@ export default {
       name: '',
       imageUrl: '',
       chapterList: [],
+      currentMenuIndex: 'chapter-0',
       currentChapterId: -1,
       content: '',
-      videoList: []
+      videoList: [],
+      showVideo: false
     });
+
+    const handleMenuItemClick = async (chapterId: number, index: number) => {
+      const learnId = router.currentRoute.value.params.learnId;
+      const learnDetail = await http.get('/api/learn/detail', { params: { learnId, chapterId } });
+      data.showVideo = false;
+      data.currentMenuIndex = `chapter-${index}`;
+      data.currentChapterId = chapterId;
+      data.content = learnDetail.data.data.content;
+    };
+
+    const handleVideoItemClick = async () => {
+      data.showVideo = true;
+      data.currentMenuIndex = `chapter-video`;
+      data.currentChapterId = -1;
+    };
+
+    const jumpToUrl = (url: string) => {
+      window.open(url);
+    };
 
     onMounted(async () => {
       const learnId = router.currentRoute.value.params.learnId;
@@ -32,6 +88,7 @@ export default {
       data.imageUrl = imageUrl;
       data.chapterList = chapterList;
       data.currentChapterId = chapterId;
+      data.currentMenuIndex = 'chapter-0';
 
       const [learnDetail, learnVideo] = await Promise.all([
         http.get('/api/learn/detail', { params: { learnId, chapterId } }),
@@ -39,22 +96,46 @@ export default {
       ]);
       data.content = learnDetail.data.data.content;
       data.videoList = learnVideo.data.data;
-
-      console.log(
-        'mounted data:',
-        chapterList,
-        chapterId,
-        learnDetail.data.data.content,
-        learnVideo.data.data
-      );
     });
 
     return {
-      ...toRefs(data)
+      ...toRefs(data),
+      handleMenuItemClick,
+      handleVideoItemClick,
+      jumpToUrl
     };
   }
 };
 </script>
 
 <style scoped>
+.learn-detail {
+  text-align: left;
+}
+.learn-detail-menu {
+  width: 200px;
+  flex-shrink: 0;
+  min-height: calc(100vh - 61px);
+}
+.learn-video {
+  flex-wrap: wrap;
+}
+.learn-video-card {
+  margin: 20px;
+  width: 300px;
+  height: 260px;
+  cursor: pointer;
+}
+.learn-video-card-image {
+  width: 300px;
+  height: 200px;
+  object-fit: cover;
+  display: block;
+}
+.learn-video-card-text {
+  margin: 20px;
+  height: 20px;
+  line-height: 20px;
+  text-align: center;
+}
 </style>
