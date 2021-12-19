686279e14e4e4ba9d78617daa4c39a3479a367b5
hefl <fengli.he@wedcm.com>
2021-12-19 19:33:27 +0800

    知识体系

---
+++ package/server/controller/knowledgeController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { KnowledgeService } from '../service/knowledgeService';
+import { MoreThan } from 'typeorm';
+import { sendSuccessResponse, sendErrorResponse } from '../util/response';
+
+export class KnowledgeController {
+  private knowledgeService = new KnowledgeService();
+
+  getKnowledge = async (req: Request, res: Response) => {
+    try {
+      
+      // 获取知识体系内容
+      const output = await this.knowledgeService.findAvailableKnowledge();
+
+      return sendSuccessResponse(res, output);
+    } catch (e) {
+      res.send(e.message);
+    }
+  };
+}
---
+++ package/server/service/knowledgeService.ts
@@ -0,0 +1,28 @@
+import { KnowledgeEntity } from '../entity/knowledgeEntity';
+import { getRepository, FindManyOptions } from 'typeorm';
+
+export class KnowledgeService {
+  private knowledgeRepository = getRepository(KnowledgeEntity);
+
+  find = async (query: FindManyOptions<KnowledgeEntity>) => {
+    return this.knowledgeRepository.find(query);
+  };
+
+  findAvailableKnowledge = async () => {
+    const availableList = await this.find({
+      where: {},
+      take: 100,
+      skip: 0
+    });
+
+		const output = availableList.map(item => {
+			return {
+				id:item.id,
+				name:item.name,
+				content:item.content
+			}
+		})
+
+    return output;
+  };
+}
