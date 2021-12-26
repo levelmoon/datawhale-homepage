e672bee35bc52f00dfd6e8b899dd717b5905ad7e
shadowings-zy <shadowingszy@outlook.com>
2021-12-26 23:01:36 +0800

    feat: 修改线上环境数据库的地址

--- package/server/config/prod.ts
+++ package/server/config/prod.ts
@@ -3,11 +3,11 @@ import path from 'path';
 export const productionConfig = {
   db: {
     type: 'mysql',
-    host: 'localhost',
+    host: 'rm-bp149d70aowb5164y0o.mysql.rds.aliyuncs.com',
     port: 3306,
     username: 'root',
     passwordPath: path.join(__dirname, '../../password.json'),
-    database: 'datawhale',
+    database: 'datawhale_homepage',
     entities: [path.join(__dirname, '../entity/**/*.js')]
   },
   staticFilePath: path.join(__dirname, '../client')
